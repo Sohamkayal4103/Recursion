9376d90a1cfba3cab8b45feb8b6c09199fbbf08e
Soham <kayalsoham61@gmail.com>
2022-03-30 09:49:53 +0530

    printNum by simple recursion

---
+++ printNum.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void printNum(ll count,ll i){
+  if(count == 0){
+    return;
+  }
+  cout<< i << endl;
+  count--;
+  i++;
+  printNum(count,i);
+}
+int main(){
+  ll n;
+  cin >> n;
+  printNum(n,1);
+  return 0;
+}
