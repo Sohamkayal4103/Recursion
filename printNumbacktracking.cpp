a811d3c51f97dfafda8ad09c49a1d78604bc5b9d
Soham <kayalsoham61@gmail.com>
2022-03-30 10:20:05 +0530

    printNum using backtarcking

---
+++ printNumbacktracking.cpp
@@ -0,0 +1,22 @@
+//printing 1 to n using backtracking
+// Language: cpp
+// Path: printNumbacktracking.cpp
+// Compare this snippet from printName.cpp:
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void printNumbacktracking(ll count){
+  if(count == 0){
+    return;
+  }
+  printNumbacktracking(count-1);
+  cout<< count << endl;
+}
+int main(){
+  ll n;
+  cin >> n;
+  printNumbacktracking(n);
+
+  return 0;
+}
